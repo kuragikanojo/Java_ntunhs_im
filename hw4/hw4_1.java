aaff1a8e9ab329be90e79b6bb59e3a9d62d42278
jackchen838 <jackhero112888@gmail.com>
2024-03-19 11:30:40 +0800

    hw4

--- hw4/hw4_1.java
+++ hw4/hw4_1.java
@@ -1,34 +1,41 @@
+import java.util.Scanner;
+
 public class hw4_1 {
 
     public static void main(String[] args) {
 
-        char[] check = {'J', 'a', 'v', 'a', '程', '式', '入', '門'}; // 建立一個 10個內容的字元陣列
+        String temp = "";
+        Scanner scanner = new Scanner(System.in);
 
-        String str1 = new String(); // 建立空字串
+            System.out.print("這是陪你聊程式，可以問我問題");
 
-        String str2 = new String(check); // 由字元陣列完整的變成字串
+            while (true) {
+                System.out.println("你說:");
+                temp = scanner.nextLine();
+                if (temp.endsWith("嗎")) {
 
-        String str3 = new String(check, 0, 4); // 由字元陣列的0號位置連續取4個字元出來(由0開始編號)
+                temp = temp.substring(0, temp.length() - 1);
 
-        String str4 = new String("宣告字串");// 由字串內容宣告字串物件
+            }
 
-        String str5 = new String(str2); // 由字串變數宣告字串物件
+                temp = temp.replace("?", "!");
 
-        System.out.println("str1 = " + str1);
+                temp = temp.replace("會不會", "會");
 
-        System.out.println("str2 = " + str2);
+                temp = temp.replace("能不能", "能");
 
-        System.out.println("str3 = " + str3);
+                temp = temp.replace("你會", "我會");
 
-        System.out.println("str4 = " + str4);
+                temp = temp.replace("你能", "我能");
+                
+                temp = temp.replace("嗎", " ");
+                
 
-        System.out.println("str5 = " + str5);
-
-        System.out.println("比較 str2 與 str5 是否一致的結果?" + (str2 == str5));
-
-        System.out.println("比較 str2 與str5 是否一致的結果?" + str2.equals(str5));
-
+                if(temp.equals("0")){
+                    System.out.println("離開聊天程序");
+                    break;
+                }
+                System.out.println("回答：" + temp);
+            }
     }
-
 }
-
